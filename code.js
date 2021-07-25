cb168cfa56ea250327ddd0579aa634836dde28af
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-07-25 21:56:55 +0200

    feat: app now can answer the question based on passage

--- code.js
+++ code.js
@@ -1,7 +1,28 @@
-// Load the model.
-qna.load().then(model => {
-    // Find the answers
-    model.findAnswers(question, passage).then(answers => {
-        console.log('Answers: ', answers);
-    });
+const $form = document.querySelector('form');
+$form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const passage = e.target.passage.value;
+    const question = e.target.question.value;
+
+    findAnswers(question, passage);
 });
+
+const findAnswers = async(question, passage) => {
+    const model = await qna.load();
+    const answers = await model.findAnswers(question, passage);
+    console.log(answers);
+
+    // select the answers div
+    const $answersDiv = document.getElementById('answers');
+
+    // empty it if there are answers from previous question
+    $answersDiv.innerHTML = '';
+
+    // append every answer to answers div
+    answers.forEach((answer, idx) => {
+        const $p = document.createElement('p');
+        $p.append(`${idx + 1}. ${answer.text}, accuracy: ${answer.score.toFixed(2)}%`);
+        $answersDiv.append($p);
+    });
+}
